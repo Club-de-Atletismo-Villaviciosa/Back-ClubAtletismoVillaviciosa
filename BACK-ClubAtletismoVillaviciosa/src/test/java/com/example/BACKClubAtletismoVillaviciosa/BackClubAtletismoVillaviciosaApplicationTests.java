46627cd7397e15487da89c2006b0de71b9da0876
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-04-25 13:35:56 +0200

    first commit

---
+++ BACK-ClubAtletismoVillaviciosa/src/test/java/com/example/BACKClubAtletismoVillaviciosa/BackClubAtletismoVillaviciosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BACKClubAtletismoVillaviciosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackClubAtletismoVillaviciosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
