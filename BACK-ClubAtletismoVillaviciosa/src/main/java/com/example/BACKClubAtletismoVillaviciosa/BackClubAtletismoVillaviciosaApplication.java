61c391d49d5be020e2e4c416dc26cba913f8af1b
Aurino04 <a_llera_rodriguez@hotmai.l.com>
2023-05-17 11:46:56 +0200

    login added

--- BACK-ClubAtletismoVillaviciosa/src/main/java/com/example/BACKClubAtletismoVillaviciosa/BackClubAtletismoVillaviciosaApplication.java
+++ BACK-ClubAtletismoVillaviciosa/src/main/java/com/example/BACKClubAtletismoVillaviciosa/BackClubAtletismoVillaviciosaApplication.java
@@ -3,11 +3,13 @@ package com.example.BACKClubAtletismoVillaviciosa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackClubAtletismoVillaviciosaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackClubAtletismoVillaviciosaApplication.class, args);
+
 	}
 
 }
---
+++ BACK-ClubAtletismoVillaviciosa/src/main/java/com/example/BACKClubAtletismoVillaviciosa/login/Repo/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.BACKClubAtletismoVillaviciosa.login.Repo;
+
+import com.example.BACKClubAtletismoVillaviciosa.login.Entity.Role;
+import com.example.BACKClubAtletismoVillaviciosa.login.enums.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(ERole name);
+}
